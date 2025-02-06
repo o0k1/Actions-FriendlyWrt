f7d1c79b752c5806e99657b0b3604e10d8414f22
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-02-06 18:10:38 +0800

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,7 +3,3 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
-
-# luci-app-mosdns requires golang 1.22.x or latest version
-rm -rf friendlywrt/feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x friendlywrt/feeds/packages/lang/golang
