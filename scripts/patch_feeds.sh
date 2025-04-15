3dac85f32d3c337ba9ebd76f8f83f44711105ff0
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-04-15 23:03:16 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -5,4 +5,4 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 # Install missing pcre libs (one possible solution)
-apt install -y libpcre3 libpcre3-dev
+sudo apt install -y libpcre3 libpcre3-dev
