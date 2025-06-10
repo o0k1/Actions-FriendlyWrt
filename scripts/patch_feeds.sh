8255b24f0092f47da5003fdf51b69304097fc9f0
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-06-10 15:48:17 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -2,4 +2,4 @@
 
 # Patch feeds for mosdns (one possible solution)
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
