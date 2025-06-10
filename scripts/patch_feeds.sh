7b204c4c5faae08148e2b4dd1c7a99dbcfa27980
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-06-10 15:39:13 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -3,7 +3,3 @@
 # Patch feeds for mosdns (one possible solution)
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-
-# Install missing pcre libs (one possible solution)
-sudo apt install -y libpcre3 libpcre3-dev
-./scripts/feeds install libpcre
