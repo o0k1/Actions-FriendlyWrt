c506f9eaf6f22919f730bcf7c87fdd772a476b5b
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-06-12 09:24:06 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -3,6 +3,3 @@
 # Patch feeds for mosdns (one possible solution)
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
-
-# Possible fix for libpcre
-rm -fr bin/ dl/ staging_dir/ build_dir/ tmp/
