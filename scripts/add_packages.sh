6d6c29d0ca20fb6a44108000721b3e0a4b45b724
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-06-27 14:06:58 +0800

    Update add_packages.sh

--- scripts/add_packages.sh
+++ scripts/add_packages.sh
@@ -65,11 +65,3 @@ CONFIG_PACKAGE_luci-i18n-vlmcsd-zh-cn=y
 CONFIG_PACKAGE_vlmcsd=y
 EOF
 # }}
-
-# {{ Add wifi driver for MT7922
-cat >> configs/rockchip/01-nanopi << EOF
-CONFIG_PACKAGE_kmod-mt792x-common=y
-CONFIG_PACKAGE_kmod-mt792x-usb=y
-CONFIG_PACKAGE_mt7922bt-firmware=y
-EOF
-# }}
