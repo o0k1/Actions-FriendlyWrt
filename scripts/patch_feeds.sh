9b9e5a9ecd5cf4096984d2eca4337b888937c767
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-08-29 09:50:04 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-# Patch feeds for mosdns (one possible solution)
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+OPENWRT_VERSION=""
+
+if [ ! -z "$1" ]; then
+	OPENWRT_VERSION="$1"
+fi
+
+if [[ "${OPENWRT_VERSION}" =~ ^23\..+$ ]]; then
+  # Patch feeds for mosdns (one possible solution)
+  rm -rf feeds/packages/lang/golang
+  git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+fi
