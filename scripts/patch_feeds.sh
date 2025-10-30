4226021ea2f20b3cf09cd7b8653446cdf18e48e2
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-10-30 19:42:10 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -11,3 +11,7 @@ if [[ "${OPENWRT_VERSION}" =~ ^23\..+$ ]]; then
   rm -rf feeds/packages/lang/golang
   git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 fi
+
+# Set Rust build arg llvm.download-ci-llvm to false.
+# Thanks to https://github.com/Shuery-Shuai/ImmortalWrt-BPI-R4-Firmware/commit/17bffb5b345fb04be81a6a3632b5c341607172fc
+sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
