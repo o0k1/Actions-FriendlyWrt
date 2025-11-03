bcc0ef542befc7821969ddad099bf7d0837c63f7
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-11-03 14:53:48 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -12,6 +12,12 @@ if [[ "${OPENWRT_VERSION}" =~ ^23\..+$ ]]; then
   git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 fi
 
+if [[ "${OPENWRT_VERSION}" =~ ^24\..+$ ]]; then
+  # Bump go version for xtls-core (always use latest)
+  rm -rf feeds/packages/lang/golang
+  git clone https://github.com/DokiDuck/openwrt-packages-lang-golang -b openwrt-master feeds/packages/lang/golang
+fi
+
 # Set Rust build arg llvm.download-ci-llvm to false.
 # Thanks to https://github.com/Shuery-Shuai/ImmortalWrt-BPI-R4-Firmware/commit/17bffb5b345fb04be81a6a3632b5c341607172fc
 sed -i 's/--set=llvm\.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/' feeds/packages/lang/rust/Makefile
