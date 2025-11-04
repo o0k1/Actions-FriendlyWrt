6d776b92efb167dac99dec25cb599f848a22c2a5
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2025-11-05 00:38:32 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -6,12 +6,6 @@ if [ ! -z "$1" ]; then
 	OPENWRT_VERSION="$1"
 fi
 
-if [[ "${OPENWRT_VERSION}" =~ ^23\..+$ ]]; then
-  # Patch feeds for mosdns (one possible solution)
-  rm -rf feeds/packages/lang/golang
-  git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-fi
-
 if [[ "${OPENWRT_VERSION}" =~ ^24\..+$ ]]; then
   # Bump go version for xtls-core (always use latest)
   rm -rf feeds/packages/lang/golang
