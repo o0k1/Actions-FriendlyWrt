69d98255d65771fe8fd15c05fcc8fb23db959ee9
Doki Duck <150113750+DokiDuck@users.noreply.github.com>
2026-02-11 10:55:20 +0800

    Update patch_feeds.sh

--- scripts/patch_feeds.sh
+++ scripts/patch_feeds.sh
@@ -9,7 +9,7 @@ fi
 if [[ "${OPENWRT_VERSION}" =~ ^24\..+$ ]]; then
   # Bump go version for xtls-core (always use latest)
   rm -rf feeds/packages/lang/golang
-  git clone https://github.com/DokiDuck/openwrt-packages-lang-golang -b openwrt-master-pin-6982d97 feeds/packages/lang/golang
+  git clone https://github.com/DokiDuck/openwrt-packages-lang-golang -b coolsnowwolf-master feeds/packages/lang/golang
 fi
 
 # Set Rust build arg llvm.download-ci-llvm to false.
